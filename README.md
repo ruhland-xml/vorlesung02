e79f216e72ccbda8a21d1fe5f5bcd3889fbf038b
ruhland-we2 <k.ruhland@hszg.de>
2022-03-23 11:17:03 +0100

    init

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## Übungen Lesen einer xml-Datei mit NodeJS
 
-Wir nutzen das npm Paket `fast-xml-parser`
+Wir nutzen das npm Paket `xml2js`
 
 ```batch
 npm init -y
-npm i -D fast-xml-parser
+npm i -D xml2js
 ```
 
-Der `fast-xml-parser` wurde gewählt, da er eine sehr hohe download-Zahl besitzt und ein aktuelles npm Paket ist.
+Der `xml2js` wurde gewählt, da er eine sehr hohe download-Zahl besitzt und ein aktuelles npm Paket ist.
 
 Ausserdem wurde eine `.gitignore` Datei angelegt mit dem Inhalt `node_modules` so dass die Pakete einfach mit `$npm update` aktualisiert werden können.
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-var parser = require('fast-xml-parser');
+var parseString = require('xml2js').parseString;
+var xml = `
+<?XML version="1.0" encoding="UTF-8" standalone="yes"?>
+<katalog>
+    <firma>
+        <name>Ruhland&Söhne GMBH</name>
+        <adresse>02826 Görlitz, Brückenstraße 1</adresse>
+        <kontakte>
+            <1>+49 177 7848970</1>
+            <2>+49 3581 374 4302</2>
+        </kontakte>
+    </firma>
+</katalog>
+`;
 
-xmlData = `
-<results>
-  <books>
-   <bookinfo>
-        <name>1</dbname>
-   </bookinfo>
-   <bookinfo>
-     <name>2</dbname>
-   </bookinfo>
-   <bookinfo>
-     <name>3</dbname>
-   </bookinfo>
- </books>
-</results>
-`
-var jsonObj = parser.parse(xmlData);
+parseString(xml, function (err, result) {
+    if ( err !== null )
+        console.log(err);
+    else
+        console.log(result);
+});
